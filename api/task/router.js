fc3ba494ed2f221f9fd455f43313b87f8d78387f
RealSadLemon <esmodearburk@gmail.com>
2022-07-22 20:20:12 -0500

    created router.js

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,14 @@
 // build your `/api/tasks` router here
+const express = require('express');
+
+const model = require('./model');
+
+const {  } = require('./middleware');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+});
+
+module.exports = router;
