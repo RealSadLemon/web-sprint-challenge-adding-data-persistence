e1c13397ae517e607dbce10571b712e8f59f3d50
RealSadLemon <esmodearburk@gmail.com>
2022-07-22 20:21:56 -0500

    Finished hooking up previously afformentioned and unspecified tasks router.js (oops)

--- api/server.js
+++ api/server.js
@@ -5,6 +5,8 @@ const projectsRouter = require('./project/router');
 
 const resourcesRouter = require('./resource/router');
 
+const tasksRouter = require('./task/router');
+
 const server = express();
 
 server.use(express.json());
@@ -12,5 +14,7 @@ server.use(express.json());
 server.use('/api/projects', projectsRouter);
 
 server.use('/api/resources', resourcesRouter);
+
+server.use('/api/tasks', tasksRouter);
 
 module.exports = server;
