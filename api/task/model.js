0b38e1eeaac5a0bdf0c7ca8e964a3dbf1494ccdf
RealSadLemon <esmodearburk@gmail.com>
2022-07-22 20:35:06 -0500

    Finished getAllTasks model functionality and by proxy finished GET endpoint

--- api/task/model.js
+++ api/task/model.js
@@ -1,8 +1,18 @@
 const db = require('../../data/dbConfig')
 
 // build your `Task` model here
-function getAllTasks() {
-
+async function getAllTasks() {
+    const tasks = await db('tasks as tsks')
+        .select('tsks.task_id', 'tsks.task_description', 'tsks.task_notes', 'tsks.task_completed', 'pr.project_name', 'pr.project_description')
+        .leftJoin('projects as pr', 'tsks.project_id', 'pr.project_id')
+        .groupBy('tsks.task_id')
+        .orderBy('tsks.task_id', 'asc')
+    return tasks.map(task => {
+        return ({
+            ...task,
+            task_completed: !!task.task_completed
+        })
+    })
 }
 
 module.exports = {
