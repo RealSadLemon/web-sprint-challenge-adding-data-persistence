b55493af67b7ae7487ce60bc25503ecd673eb4ea
RealSadLemon <esmodearburk@gmail.com>
2022-07-22 19:35:22 -0500

    Created projects router and functionality for GET in said router

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,20 @@
+const db = require('../../data/dbConfig');
+
 // build your `Project` model here
+
+async function getAllProjects() {
+    const projects = await db('projects')
+    return projects.map(project => {
+        return ({
+            project_id: project.project_id,
+            project_name: project.project_name,
+            project_description: project.project_description,
+            project_completed: !!project.project_completed
+        })
+    })
+    
+}
+
+module.exports = {
+    getAllProjects
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,15 @@
 // build your `/api/projects` router here
+const express = require('express');
+
+const model = require('./model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    model.getAllProjects()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+})
+
+module.exports = router
