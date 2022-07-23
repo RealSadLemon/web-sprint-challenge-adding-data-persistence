c966dd410cced9471e73bc3a7802a67e098b32d5
RealSadLemon <esmodearburk@gmail.com>
2022-07-22 19:59:43 -0500

    created GET for /api/resources endpoint

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,15 @@
 // build your `/api/resources` router here
+const express = require('express');
+
+const model = require('./model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    model.getAllResources()
+        .then(resources => {
+            res.status(200).json(resources)
+        })
+})
+
+module.exports = router;
